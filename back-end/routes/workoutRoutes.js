4f659d379c2075cb775d4c472ce8d5b6833d92fa
Yolanda Mejane <mysivuyile@gmail.com>
2025-04-09 15:23:13 +0200

    missing put and delete functions

--- back-end/routes/workoutRoutes.js
+++ back-end/routes/workoutRoutes.js
@@ -24,4 +24,45 @@ router.post('/', authMiddleware, async (req, res) => {
   }
 });
 
+router.put('/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const { exercise, reps, sets, weight, date } = req.body;
+
+  try {
+    const workout = await Workout.findOne({ _id: id, userId: req.user.userId });
+
+    if (!workout) {
+      return res.status(404).json({ message: 'Workout not found or you are not authorized to update this workout' });
+    }
+
+    workout.exercise = exercise;
+    workout.reps = reps;
+    workout.sets = sets;
+    workout.weight = weight;
+    workout.date = date;
+
+    await workout.save();
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ message: 'Failed to update workout' });
+  }
+});
+
+router.delete('/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const workout = await Workout.findOne({ _id: id, userId: req.user.userId });
+
+    if (!workout) {
+      return res.status(404).json({ message: 'Workout not found or you are not authorized to delete this workout' });
+    }
+
+    await workout.remove();
+    res.status(200).json({ message: 'Workout deleted successfully' });
+  } catch (err) {
+    res.status(400).json({ message: 'Failed to delete workout' });
+  }
+});
+
 export default router;
